5e9a5f9db49a517c0b616d2da7c9b102c2a55b9b
Sriram Kailasam <sriramklash@gmail.com>
2018-08-26 11:48:47 +0530

    Separate queries for name and temperature data. Use async/await instead of promises

--- app/controllers/water_body.controller.ts
+++ app/controllers/water_body.controller.ts
@@ -1,53 +1,59 @@
+import {WaterBody} from './../models/water_body.model';
 import {Request, Response} from 'express';
 import {DbClient} from '../server';
-import {WaterBody} from '../models/water_body.model';
 import {TemperatureInfo} from '../models/temperature_info.model';
+import {QueryResult} from 'pg';
 
 export class WaterBodyController {
-	static listAllWaterBodies(req: Request, res: Response) {
+	static async listAllWaterBodies(req: Request, res: Response) {
 		const query = 'SELECT id, name FROM water_bodies';
 		let waterBodyList: Array<WaterBody> = new Array();
 
-		DbClient.query(query)
-		.then(result => {
-			for (let row of result.rows) {
-				waterBodyList.push(new WaterBody(Number(row.id), row.name));
-			}
+		let queryResult = await DbClient.query(query);
+		for (let row of queryResult.rows) {
+			waterBodyList.push(new WaterBody(Number(row.id), row.name));
+		}
 
-			res.render('water_body_list', {waterBodyList: waterBodyList});
-		})
-		.catch(err => console.error(err));
+		res.render('water_body_list', {waterBodyList: waterBodyList});
 	}
 
-	static showWaterBody(req: Request, res: Response) {
+	static async showWaterBody(req: Request, res: Response) {
 		let id = req.params.id;
 
-		const query =
-			`SELECT water_bodies.id, `
-			+ `water_bodies.name, `
-			+ `temperature_data.minimum_temperature, `
-			+ `temperature_data.maximum_temperature `
-			+ `FROM water_bodies INNER JOIN temperature_data ON `
-			+ `water_bodies.id=temperature_data.water_body_id `
-			+ `AND temperature_data.water_body_id=${id}`;
+		const nameQuery = 'SELECT id, name FROM water_bodies WHERE id=$1';
+		const temperatureQuery =
+			'SELECT temperature_data.minimum_temperature, '
+			+ 'temperature_data.maximum_temperature '
+			+ 'FROM water_bodies INNER JOIN temperature_data ON '
+			+ 'water_bodies.id=temperature_data.water_body_id '
+			+ 'AND temperature_data.water_body_id=$1';
 
-		DbClient.query(query)
-		.then(result => {
+		let nameQueryResult: QueryResult = await DbClient.query(nameQuery, [id]);
+		let temperatureQueryResult: QueryResult = await DbClient.query(temperatureQuery, [id]);
 
-			let waterBody = new WaterBody(Number(result.rows[0].id), result.rows[0].name);
-			let temperatureData: Array<TemperatureInfo> = Array();
+		if (nameQueryResult.rowCount == 0) {
+			res.status(404).end();
+			console.error('Could not retreive name of water body');
+			return;
+		}
 
-			for (let row of result.rows) {
-				let temperatureInfo =
-				new TemperatureInfo(
+		let waterBody =
+			new WaterBody(Number(nameQueryResult.rows[0].id), nameQueryResult.rows[0].name);
+
+		let temperatureData: Array<TemperatureInfo> = new Array();
+
+		if (temperatureQueryResult.rowCount != 0) {
+			for (let row of temperatureQueryResult.rows) {
+				let temperatureInfo = new TemperatureInfo(
 					Number(row.minimum_temperature),
 					Number(row.maximum_temperature),
-					Number(id));
+					Number(row.water_body_id)
+				);
+
 				temperatureData.push(temperatureInfo);
 			}
+		}
 
-			res.render('water_body', {waterBody: waterBody, temperatureData: temperatureData});
-		})
-		.catch(err => console.error(err));
+		res.render('water_body', {waterBody: waterBody, temperatureData: temperatureData});
 	}
 }

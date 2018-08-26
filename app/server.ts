a747fc9d5497a7ba74eb10d8ee8138f5aad92815
Sriram Kailasam <sriramklash@gmail.com>
2018-08-26 11:46:32 +0530

    Add database name and user name to connection options

--- app/server.ts
+++ app/server.ts
@@ -11,6 +11,8 @@ const app: express.Application = express();
 const dbConnectionConfig: ClientConfig = {
 	host: process.env.DB_HOST,
 	port: Number(process.env.DB_PORT),
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	keepAlive: true
 };

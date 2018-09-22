09690af111ebe6ef4cb87cb90de2b544dd01d413
sriram <sriramklash@gmail.com>
2018-09-22 10:21:01 +0530

    Add debug statements

--- app/socket_event_dispatcher.ts
+++ app/socket_event_dispatcher.ts
@@ -5,39 +5,51 @@ import {HumidityInfo} from './models/humidity_info.model';
 
 export class SocketEventDispactcher {
 	async registerHandlers(server: Server) {
+		await DbClient.query("LISTEN temperature_change_channel");
+		
 		server.on('connection', (socket) => {
+			console.log('Socket client connected');
 			DbClient.on('notification', (message) => {
+				console.log(message);
 				switch (message.channel) {
-					case 'temperature_change_channel':
+					case 'temperature_change_channel': {
 						this.dispatchTemperatureChangeEvent(socket, JSON.stringify(message.payload));
-					case 'humidity_change_channel':
+						break;
+					}
+					case 'humidity_change_channel': {
 						this.dispatchHumidityChangeEvent(socket, JSON.stringify(message.payload));
+						break;
+					}
 				}
 			});
 		});
 	}
 
-	private async dispatchTemperatureChangeEvent(socket: Socket, data: string) {
+	private dispatchTemperatureChangeEvent(socket: Socket, data: string) {
 		let row = JSON.parse(data);
 
-		let date: string = this.getDateFromPayload(row);
-		let time: string = this.getTimeFromPayload(row);
+		console.log(row);
+
+		let date: string = this.getDateFromPayload(data);
+		let time: string = this.getTimeFromPayload(data);
 
 		let temperatureInfo = new TemperatureInfo(
 			date,
 			time,
-			Number(row.minimum_temperature),
-			Number(row.maximum_temperature),
-			Number(row.water_body_id));
+			Number(row["minimum_temperature"]),
+			Number(row["maximum_temperature"]),
+			Number(row["water_body_id"])
+		);
 
-		socket.emit('temperature_changed', temperatureInfo.toString());
+	socket.emit('temperature_changed', temperatureInfo.toString());
+		console.log(`Data sent: ${temperatureInfo.toString()}`);
 	}
 
-	private async dispatchHumidityChangeEvent(socket: Socket, data: string) {
+	private dispatchHumidityChangeEvent(socket: Socket, data: string) {
 		let row = JSON.parse(data);
 
-		let date: string = this.getDateFromPayload(row);
-		let time: string = this.getTimeFromPayload(row);
+		let date: string = this.getDateFromPayload(data);
+		let time: string = this.getTimeFromPayload(data);
 		
 		let humidityInfo = new HumidityInfo(
 			date,
@@ -47,15 +59,16 @@ export class SocketEventDispactcher {
 		);
 
 		socket.emit('humidity_changed', humidityInfo.toString());
+		console.log('Data sent');
 	}
 
-	private getDateFromPayload(payload: any): string {
+	private getDateFromPayload(payload: string): string {
 		let dateRegex = new RegExp(/[0-9]{4}-[0-9]{2}-[0-9]{2}/);
-		return dateRegex.exec(payload.datetime)![0].split('-').reverse().join('-');
+		return dateRegex.exec(payload)![0].split('-').reverse().join('-');
 	}
 
-	private getTimeFromPayload(payload: any): string {
+	private getTimeFromPayload(payload: string): string {
 		let timeRegex = new RegExp(/[0-9]{2}:[0-9]{2}:[0-9]{2}/);
-		return timeRegex.exec(payload.datetime)![0];
+		return timeRegex.exec(payload)![0];
 	}
 }

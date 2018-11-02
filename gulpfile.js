77843e5df89b662541d0761c8b1796b5b2c50707
sriram <sriramklash@gmail.com>
2018-11-03 00:26:51 +0530

    Copy scripts and views into dist.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const terser = require('gulp-terser');
+
+function minifyJs() {
+	return gulp.src('app/public/scripts/*.js').
+		pipe(terser()).
+		pipe(gulp.dest('dist/public/scripts'))
+}
+
+function views() {
+	return gulp.src('app/views/*.pug')
+		.pipe(gulp.dest('dist/views'));
+}
+
+exports.default = gulp.parallel(minifyJs, views);
